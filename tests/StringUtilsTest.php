fe030f791348b10f928d62eb7a9a0f2cb7c8b33a
AntonKorob <gorizont102.k@mail.ru>
2025-03-21 13:49:15 +0700

    add filesistem

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,18 +1,25 @@
 <?php
 require __DIR__ . ("/../vendor/autoload.php");
 require __DIR__ . ("/../src/StringUtils.php");
+
 use Webmozart\Assert\Assert;
 
 // Assert::notEq(true, false);
 
+Assert::eq(StringUtils\capitalize('hello'), 'Hello');
+Assert::eq(StringUtils\capitalize(''), '');
+
+// Assert::notEq(StringUtils\capitalize('hello'), 'Hello');
+// Assert::notEq(StringUtils\capitalize(''), '');
+
 // if (StringUtils\capitalize('hello') !== 'Hello') {
 //     throw new \Exception('Функция работает неверно!');
 // }
-Assert::notEq(StringUtils\capitalize('hello'), 'Hello');
-Assert::notEq(StringUtils\capitalize(''), '');
 
 // if (StringUtils\capitalize('') !== '') {
 //     throw new \Exception('Функция работает неверно!');
 // }
+// Assert::string( 1, 'string');
+
 
 echo 'Все тесты пройдены!';
--- tests/methodsTest.php
+++ tests/methodsTest.php
@@ -8,4 +8,20 @@ require __DIR__ . ("/../vendor/autoload.php");
 
 use Webmozart\Assert\Assert;
 
-Assert::notEq(true, false);
+// Assert::notEq(true, false);
+function take($collection, $length = 1)
+{
+    return array_slice($collection, 0, $length);
+}
+
+// Напишите тесты для функции take($items, $n), которая возвращает первые n элементов из массива. По умолчанию n равен 1.
+
+// take([], 2); // []
+// take([1, 2, 3]); // [1]
+// take([1, 2, 3], 2); // [1, 2]
+// take([4, 3], 9); // [4, 3]
+
+Assert::eq(take([], 2), $message = []);
+Assert::eq(take([1, 2, 3]), $message = [1]);
+Assert::eq(take([1, 2, 3], 2), $message = [1,2]);
+Assert::eq(take([4, 3], 9), $message = [4,3]);
